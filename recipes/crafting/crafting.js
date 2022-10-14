a6a821ade59ee2b1ce812d15671552270fc259ff
emblo <emma.blomqvist@outlook.com>
2022-10-14 18:23:17 +0200

    crafting setup done

---
+++ recipes/crafting/crafting.js
@@ -0,0 +1,73 @@
+//Export to be used elsewhere
+
+//Array of objects containing Refined Materials crafting recipes in DDV
+let refinedMaterials = [
+    {
+        "Item": "",
+        "Material1": "",
+        "Material2": "",
+        "Material3": "",
+        "Material4": "",
+        "Material5": ""
+    },
+]
+
+//Array of objects containing Enchantments crafting recipes in DDV
+let enchantments = [
+    {
+        "Item": "",
+        "Material1": "",
+        "Material2": "",
+        "Material3": "",
+        "Material4": "",
+        "Material5": ""
+    },
+]
+
+//Array of objects containing Fences crafting recipes in DDV
+let fences = [
+    {
+        "Item": "",
+        "Material1": "",
+        "Material2": "",
+        "Material3": "",
+        "Material4": "",
+        "Material5": ""
+    },
+]
+
+//Array of objects containing Paving crafting recipes in DDV
+let paving = [
+    {
+        "Item": "",
+        "Material1": "",
+        "Material2": "",
+        "Material3": "",
+        "Material4": "",
+        "Material5": ""
+    },
+]
+
+//Array of objects containing Functional Items crafting recipes in DDV
+let functionalItems = [
+    {
+        "Item": "",
+        "Material1": "",
+        "Material2": "",
+        "Material3": "",
+        "Material4": "",
+        "Material5": ""
+    },
+]
+
+//Array of objects containing Furniture crafting recipes in DDV
+let furniture = [
+    {
+        "Item": "",
+        "Material1": "",
+        "Material2": "",
+        "Material3": "",
+        "Material4": "",
+        "Material5": ""
+    },
+]
