7b1c2b8cc833a6f3916fa8702059711dcbd89fb5
emblo <emma.blomqvist@outlook.com>
2022-10-14 18:28:02 +0200

    characters setup done

---
+++ characters/characters.js
@@ -0,0 +1,11 @@
+//Export to use elsewhere 
+
+//Array of objects over the characters currently in DDV
+let characters = [
+    {
+        "Name": "",
+        "Movie": "",
+        "Info": "",
+        "Hangout": ""
+    }
+]
