812ded8032183ce0689f877406bb4005c921b0a4
emblo <emma.blomqvist@outlook.com>
2022-10-14 18:17:37 +0200

    ingredients setup

---
+++ recipes/cooking/ingredients.js
@@ -0,0 +1,67 @@
+//Array of objects containing Vegetables in DDV
+let vegetables = [
+    {
+        "Vegetable": "",
+        "Location": "",
+        "seedPrice": "",
+        "Energy": "",
+        "sellPrice": "",
+        "growTime" : "",
+        "harvestAmount" : ""
+    },
+]
+
+//Array of objects containing Fruit in DDV
+let fruit = [
+    {
+        "Fruit": "",
+        "Location": "",
+        "Energy": "",
+        "sellPrice": "",
+        "growTime" : "",
+        "harvestAmount" : ""
+    },
+]
+
+//Array of objects containing Grains in DDV
+let grains = [
+    {
+        "Grain": "",
+        "Location": "",
+        "seedPrice": "",
+        "Energy": "",
+        "sellPrice": "",
+        "growTime" : "",
+        "harvestAmount" : ""
+    },
+]
+
+//Array of objects containing Chez Remy Ingredients in DDV
+let chezRemyIngredients= [
+    {
+        "chezRemyIngredient": "",
+        "Location": "",
+        "Energy": "",
+        "sellPrice": "",
+    },
+]
+
+//Array of objects containing Spices in DDV
+let spices = [
+    {
+        "Spices": "",
+        "Location": "",
+        "Energy": "",
+        "sellPrice": "",
+    },
+]
+
+//Array of objects containing Shellfish in DDV
+let shellfish = [
+    {
+        "Shellfish": "",
+        "Location": "",
+        "Energy": "",
+        "sellPrice": "",
+    },
+]
