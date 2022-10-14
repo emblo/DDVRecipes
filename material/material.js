a2aa18273df87db673d4351609a04d01d9f06288
emblo <emma.blomqvist@outlook.com>
2022-10-14 18:17:17 +0200

    ingredients setup done + some material setup

---
+++ material/material.js
@@ -0,0 +1,19 @@
+//Export to be used elsewhere
+
+//Array of objects containing Crafting Materials in DDV
+let craftingMaterials = [
+    {
+        "Material": "",
+        "Location": "",
+        "sellPrice": "",
+    },
+]
+
+//Array of objects containing Flowers in DDV
+let flowers = [
+    {
+        "Flowers": "",
+        "Location": "",
+        "sellPrice": "",
+    },
+]
